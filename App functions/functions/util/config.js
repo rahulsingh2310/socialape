cb2160d8ae4971100fa3bf1653922f2e571d6a95
rahulsingh2310 <rahulkumar.s17@iiits.in>
2020-12-28 03:27:55 +0530

    First commit

---
+++ App functions/functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyCyMwwINRZTWdBcqtw-SielI0O11HkWts0",
+  authDomain: "socialmediaape-a63d5.firebaseapp.com",
+  databaseURL: "https://socialmediaape-a63d5-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "socialmediaape-a63d5",
+  storageBucket: "socialmediaape-a63d5.appspot.com",
+  messagingSenderId: "938718070149",
+  appId: "1:938718070149:web:2253b818a5943d5660b0ec",
+  measurementId: "G-2L009QJMFY"
+};
